eb5a2040f803379253f7709d7f1a1e0956e1b9b8
mtlpks <mtlpks@outlook.com>
2023-08-21 18:54:25 -0300

    Fixes layout issues on mobile

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -18,8 +18,10 @@ function CardExampleCard({ weatherData }) {
             width={"100px"}
           />
         </div>
-        <p>{moment().format("dddd")}</p>
-        <p>{moment().format("LL")}</p>
+        <div className="flex gap-4" id="date-and-day">
+          <p>{moment().format("dddd")}</p>
+          <p>{moment().format("LL")}</p>
+        </div>
         {/* <p>
           Sunrise:{" "}
           {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString("en-IN")}
@@ -28,7 +30,10 @@ function CardExampleCard({ weatherData }) {
           Sunset:{" "}
           {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString("en-IN")}
         </p> */}
-        <p>{weatherData.weather[0].description[0].toUpperCase() + weatherData.weather[0].description.substring(1)}</p>
+        <p>
+          {weatherData.weather[0].description[0].toUpperCase() +
+            weatherData.weather[0].description.substring(1)}
+        </p>
         <p>Humidity: {weatherData.main.humidity} %</p>
       </div>
     </div>
